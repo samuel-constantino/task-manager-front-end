88a86d715ae77f2f35548bc11de1a4be1d849f34
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-05 14:23:17 -0300

    refactor: separa estilo do formulário de login

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -14,6 +14,17 @@ import {
 import { AccountCircle } from '@mui/icons-material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import {
+  mainContainerPkg,
+  boxLoginPkg,
+  avatarPkg,
+  titleLoginPkg,
+  boxFormPkg,
+  emailFieldPkg,
+  passwordFieldPkg,
+  buttonSubmitPkg,
+} from './styles';
+
 const theme = createTheme();
 
 export default function SignIn() {
@@ -27,58 +38,34 @@ export default function SignIn() {
 
   return (
     <ThemeProvider theme={ theme }>
-      <Container component="main" maxWidth="xs">
+      <Container { ...mainContainerPkg }>
         <CssBaseline />
-        <Box
-          sx={ {
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          } }
-        >
-          <Avatar sx={ { m: 1, bgcolor: 'primary.main' } }>
+
+        <Box sx={ boxLoginPkg }>
+          <Avatar sx={ avatarPkg }>
             <AccountCircle />
           </Avatar>
-          <Typography component="h1" variant="h5">
-            Entrar
-          </Typography>
-          <Box component="form" onSubmit={ handleSubmit } noValidate sx={ { mt: 1 } }>
+
+          <Typography { ...titleLoginPkg }> Entrar </Typography>
+
+          <Box { ...boxFormPkg } sx={ { mt: 1 } } onSubmit={ handleSubmit }>
             <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email"
-              name="email"
-              autoFocus
+              { ...emailFieldPkg }
               value={ email }
               onChange={ ({ target }) => setEmail(target.value) }
             />
             <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Senha"
-              type="password"
-              id="password"
+              { ...passwordFieldPkg }
               value={ password }
               onChange={ ({ target }) => setPassword(target.value) }
             />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={ { mt: 3, mb: 2 } }
-            >
-              Entrar
-            </Button>
+            <Button { ...buttonSubmitPkg } sx={ { mt: 3, mb: 2 } }> Entrar </Button>
             <Link href="/register" variant="body2">
               Não tem uma conta? Inscreva-se
             </Link>
           </Box>
         </Box>
+
       </Container>
     </ThemeProvider>
   );
---
+++ src/Pages/SignIn/styles.js
@@ -0,0 +1,56 @@
+const mainContainerPkg = {
+  component: 'main',
+  maxWidth: 'xs',
+};
+const boxLoginPkg = {
+  marginTop: 8,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+};
+const avatarPkg = {
+  m: 1,
+  bgcolor: 'primary.main',
+};
+const titleLoginPkg = {
+  component: 'h1',
+  variant: 'h5',
+};
+const boxFormPkg = {
+  component: 'form',
+  noValidate: true,
+};
+const emailFieldPkg = {
+  margin: 'normal',
+  required: true,
+  fullWidth: true,
+  id: 'email',
+  label: 'Email',
+  name: 'email',
+  autoFocus: true,
+};
+const passwordFieldPkg = {
+  margin: 'normal',
+  required: true,
+  fullWidth: true,
+  name: 'password',
+  label: 'Senha',
+  type: 'password',
+  id: 'password',
+};
+const buttonSubmitPkg = {
+  type: 'submit',
+  fullWidth: true,
+  variant: 'contained',
+};
+
+module.exports = {
+  mainContainerPkg,
+  boxLoginPkg,
+  avatarPkg,
+  titleLoginPkg,
+  boxFormPkg,
+  emailFieldPkg,
+  passwordFieldPkg,
+  buttonSubmitPkg,
+};

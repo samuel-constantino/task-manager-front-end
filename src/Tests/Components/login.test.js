8833ed45ed6285829e245ef71ffa849f2c6b7bc0
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-09 08:55:28 -0300

    feat: implementa testes unitários pra login.
    Testa funcionalidade de habilitar/desabilitar botão

--- src/Tests/Components/login.test.js
+++ src/Tests/Components/login.test.js
@@ -1,4 +1,5 @@
 import { cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouter } from '../helpers/renderWithRouter';
 import Login from '../../Pages/Login/index';
 import '@testing-library/jest-dom';
@@ -10,7 +11,7 @@ describe('Verifica renderização e funcionamento dos componentes da página de 
 
     afterEach(cleanup);
 
-    describe('Basic component rendering', () => {
+    describe('Component rendering', () => {
         it('Title Heading', () => {
             const title = screen.getByRole('heading', { name: 'Entrar' });
             expect(title).toBeInTheDocument();
@@ -27,10 +28,63 @@ describe('Verifica renderização e funcionamento dos componentes da página de 
         });
 
         it('Submit Button', () => {
-            const submit = screen.getByRole('button', { name: 'Entrar' });
-            expect(submit).toBeInTheDocument();
+            const submitBtn = screen.getByRole('button', { name: 'Entrar' });
+            expect(submitBtn).toBeInTheDocument();
         });
     });
 
-});
+    describe('Components functionality', () => {
+        it('Submit button initially disabled', () => {
+            const submitBtn = screen.getByRole('button', { name: 'Entrar' });
+            expect(submitBtn).toBeDisabled();
+        });
 
+        it('Submit button remains disabled when entering email', () => {
+            const submitBtn = screen.getByRole('button', { name: 'Entrar' });
+            const emailInput = screen.getByLabelText(/email/i);
+
+            expect(submitBtn).toBeDisabled();
+
+            userEvent.type(emailInput, 'user@email.com');
+
+            expect(submitBtn).toBeDisabled();
+        });
+
+        it('Submit button remains disabled when entering password', () => {
+            const submitBtn = screen.getByRole('button', { name: 'Entrar' });
+            const passwordInput = screen.getByLabelText(/senha/i);
+
+            expect(submitBtn).toBeDisabled();
+
+            userEvent.type(passwordInput, '123456');
+
+            expect(submitBtn).toBeDisabled();
+        });
+
+        it('Submit button remains disabled when entering incorrect email and password', () => {
+            const submitBtn = screen.getByRole('button', { name: 'Entrar' });
+            const emailInput = screen.getByLabelText(/email/i);
+            const passwordInput = screen.getByLabelText(/senha/i);
+
+            expect(submitBtn).toBeDisabled();
+
+            userEvent.type(emailInput, 'useremail.com');
+            userEvent.type(passwordInput, '123456');
+
+            expect(submitBtn).toBeDisabled();
+        });
+
+        it('Submit button enabled when entering correct email and password', () => {
+            const submitBtn = screen.getByRole('button', { name: 'Entrar' });
+            const emailInput = screen.getByLabelText(/email/i);
+            const passwordInput = screen.getByLabelText(/senha/i);
+
+            expect(submitBtn).toBeDisabled();
+
+            userEvent.type(emailInput, 'user@email.com');
+            userEvent.type(passwordInput, '123456');
+
+            expect(submitBtn).toBeEnabled();
+        });
+    });
+});

6a6d241257e8dddc276f4d7a0edb2efb1700d584
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-25 07:50:57 -0300

    refactor: formulário add tarefa.
    Modifica tipos de inputs

--- src/Components/RadioWrapper/index.jsx
+++ src/Components/RadioWrapper/index.jsx
@@ -7,12 +7,20 @@ import PropTypes from 'prop-types';
 export default function RadioWrapper({ buttonsInfo }) {
   const { defaultValue, setValue, options, labels } = buttonsInfo;
 
+  const handleChange = (value) => {
+    if(value === 'true' || value === 'false'){
+      setValue(value === 'true');
+    }else{
+      setValue(value);
+    }
+  };
+
   return (
     <RadioGroup
       row
       name="controlled-radio-buttons-group"
       value={ defaultValue }
-      onChange={ ({ target }) => setValue(target.value === 'true') }
+      onChange={ ({ target }) => handleChange(target.value) }
     >
       {
         options.map((option, index) => (
